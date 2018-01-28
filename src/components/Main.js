9bfe055ebe1d3734b73ac8b45caded2750477a7c
Jarrod <Demonico@users.noreply.github.com>
2018-01-28 02:11:42 -0700

    removed formic, set up filters, set up sorting

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,23 +8,80 @@ import SearchForm from './SearchForm';
 import SearchResults from './SearchResults';
 
 export default class Main extends Component {
-  state = {
-    beer: []
-  };
+  constructor() {
+    super();
+    this.getBeers = this.getBeers.bind(this);
+    this.sortList = this.sortList.bind(this);
+    this.state = {
+      beer: []
+    };
+  }
+
 
   componentWillMount() {
     this.getBeers();
   };
 
-  getBeers() {
-    axios.get('https://api.punkapi.com/v2/beers/')
-      .then(response => {
-        this.setState({ beer: response.data }, () => {
-          console.log(this.state);
+  getBeers(searchParams = {}) {
+    console.log(searchParams);
+    const baseURL = 'https://api.punkapi.com/v2/beers/';
+    // asign value of each entry
+    const { searchField = '', searchType = 'beer_name' } = searchParams;
+    // conditionally change search url
+    const searchText = searchField.split(' ').join('_');
+    const searchURL = `${baseURL}?${searchType}=${searchText}`
+    console.log(searchURL);
+    if (searchText !== '') {
+      axios.get(searchURL)
+        .then(response => {
+          this.setState({ beer: response.data })
         })
-      })
-      .catch(err => console.log(err));
+        .catch(err => console.log(err));
+    }
   };
+
+  // function for dynamic number or string sorting
+  compareValues(key, order = 'asc') {
+    return (a, b) => {
+      if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+        // property doesn't exist on either object
+        return 0;
+      }
+
+      const varA = (typeof a[key] === 'string') ?
+        a[key].toUpperCase() : a[key];
+      const varB = (typeof b[key] === 'string') ?
+        b[key].toUpperCase() : b[key];
+
+      let comparison = 0;
+      if (varA > varB) {
+        comparison = 1;
+      } else if (varA < varB) {
+        comparison = -1;
+      }
+      return (
+        (order == 'desc') ? (comparison * -1) : comparison
+      );
+    };
+  }
+
+  // date sort
+  dateSort (key) {
+    return (a, b) => {
+      const varA = new Date('01/' + a[key]);
+      const varB = new Date('01/' + b[key]);
+      return varA < varB ? -1 : varA > varB ? 1 : 0;
+    }
+  }
+  
+
+  sortList(sort) {
+    if (sort !== 'none') {
+      sort === 'first_brewed' ? 
+      this.setState(this.state.beer.sort(this.dateSort(sort))) : 
+      this.setState(this.state.beer.sort(this.compareValues(sort)));
+    }
+  }
 
   render() {
     const beerItems = this.state.beer.map((b) => {
@@ -34,12 +91,15 @@ export default class Main extends Component {
           {/* <BeerModal beer={b} /> */}
         </div>
       )
-    });
+    })
     return (
       <div>
         <Header />
         <div className="container">
-          <SearchForm getBeers={this.getBeers} />
+          <SearchForm
+            getBeers={this.getBeers}
+            sortList={this.sortList}
+          />
         </div>
         <div className="row">{beerItems}</div>
         <Footer />
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,75 +1,106 @@
-import React from 'react';
-import { withFormik, Form, Field } from 'formik';
-import Yup from 'yup';
+import React, { Component } from 'react';
 
-const SearchForm = ({
-  values,
-  handleChange
-}) => (
-  <div>
-    <Form className="row">
-      <div className="input-group mb-3">
-        <Field
-          className="form-control-lg col"
-          type="text"
-          name="searchField"
-          placeholder="Search"
-        />
-        <div className="input-group-append">
-          <button type="submit" className="btn btn-success btn-lg">Search</button>
-        </div>
-      </div>
-      <div className="form-group col">
-        <div className="form-check form-check-inline">
-          <label className="form-check-label">
-            <Field
-              className="form-check-input form-control-lg"
-              type="radio"
-              value="beer"
-              name="searchType"
-              checked={values.searchType === "beer"}
-            />
-            Search by beer name.</label>
-        </div>
-        <div className="form-check form-check-inline">
-          <label className="form-check-label">
-            <Field
-              className="form-check-input form-control-lg"
-              type="radio"
-              value="food"
-              name="searchType"
-              checked={values.searchType === "food"}
-            />
-            Search by food pairing.</label>
-        </div>
-      </div>
-      <div className="input-group mb-3 col">
-        <Field
-          component="select"
-          name="sortList"
-          className="form-control-lg"
-        >
-          <option value="none">None</option>
-          <option value="beerName">Beer Name</option>
-          <option value="brewDate">First Brewed Date</option>
-          <option value="abv">Alcohol by Volume</option>
-        </Field>
-      </div>
-    </Form>
-  </div>
-)
-
-const FormikSearch = withFormik({
-  mapPropsToValues({ searchField, searchType, sortList }) {
-    return {
-      searchField: searchField || '',
-      searchType: searchType || 'beer',
-      sortList: sortList || 'none'
-    }
-  },
-  handleSubmit(values) {
-    console.log(values)
+export default class SearchForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchField: '',
+      searchType: 'beer_name',
+      sortList: 'none',
+      error: ''
+    };
   }
-})(SearchForm)
 
-export default FormikSearch;
+  onChange
+  handleInputChange = (e) => {
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  };
+
+  // onSort
+  onSort = (e) => {
+    const sortList = e.target.value;
+    this.setState({sortList});
+    this.props.sortList(sortList);
+  };
+  // onSubmit
+  onSubmit = (e) => {
+    e.preventDefault();
+    console.log(this.state.searchType)
+    if (!this.state.searchField) {
+      this.setState(() => ({ error: 'Please enter a search term.' }));
+    } else {
+      this.setState(() => ({ error: '' }));
+      const searchParams = {
+        searchField: this.state.searchField,
+        searchType: this.state.searchType
+      };
+      this.props.getBeers(searchParams);
+    }
+  };
+
+  render() {
+    return (
+      <div className="row">
+        <form onSubmit={this.onSubmit}>
+          <div className="input-group mb-3">
+            <input
+              className="form-control-lg col"
+              type="text"
+              name="searchField"
+              placeholder="Search"
+              value={this.state.searchField}
+              onChange={this.handleInputChange}
+            />
+            <div className="input-group-append">
+              <button type="submit" className="btn btn-success btn-lg">Search</button>
+            </div>
+          </div>
+          <div className="form-group col">
+            <div className="form-check form-check-inline">
+              <label className="form-check-label">
+                <input
+                  className="form-check-input form-control-lg"
+                  type="radio"
+                  value="beer_name"
+                  name="searchType"
+                  checked
+                  onChange={this.handleInputChange}
+                />
+                Search by beer name.</label>
+            </div>
+            <div className="form-check form-check-inline">
+              <label className="form-check-label">
+                <input
+                  className="form-check-input form-control-lg"
+                  type="radio"
+                  value="food"
+                  name="searchType"
+                  onChange={this.handleInputChange}
+                />
+                Search by food pairing.</label>
+            </div>
+          </div>
+          <div className="input-group mb-3 col">
+            <select             
+              className="form-control-lg"
+              name="sortList"
+              value={this.state.sortList}
+              onChange={this.onSort}
+            >
+              <option value="none">None</option>
+              <option value="name">Beer Name</option>
+              <option value="first_brewed">First Brewed Date</option>
+              <option value="abv">Alcohol by Volume</option>
+            </select>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
